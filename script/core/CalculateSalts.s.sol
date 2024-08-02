8b4000e45a47cc9360af236db60745056b491441
0xCLARITY <hans@royal.io>
2024-08-02 12:47:01 -0700

    Fix code comments in Provenance system contracts (#3)
    Unfortunately, changing code comments changes the bytecode - but luckily
    I caught these before protocol launch.

--- script/core/CalculateSalts.s.sol
+++ script/core/CalculateSalts.s.sol
@@ -23,19 +23,19 @@ contract CalculateSalts is Script {
     address public constant ID_REGISTRY = 0x00000000F74144b0dF049137A0F9416a920F2514;
 
     // NOTE: Needs to be filled in after salt and address calculation.
-    address public constant PROVENANCE_REGISTRY = 0x00000000956fF4AD0c5b076fB77C23a2B0EaD0D9;
+    address public constant PROVENANCE_REGISTRY = 0x00000000ec5beA5022AfDCB7b5d70026F30c0431;
 
     // Salts for sanity checking CREATE2 addresses.
     bytes32 idRegistrySalt = 0x0000000000000000000000000000000000000000fc16b93e000f0000cacf55a5;
     bytes32 idGatewaySalt = 0x000000000000000000000000000000000000000059224fd23c24440000a0154b;
     bytes32 usernameGatewaySalt = 0x000000000000000000000000000000000000000007fe84df4f04000065c41748;
-    bytes32 provenanceRegistrySalt = 0xb8a29590b1aa7ed6b776af01fb6d2589fdf0b3dad2f7c534f635c32353625d66;
-    bytes32 provenanceGatewaySalt = 0x4da199bca75deebf5eade60beb622777b64ae981713e8d7700b273d5058a2b3f;
+    bytes32 provenanceRegistrySalt = 0x0000000000000000000000000000000000000000c1638af5854f000025fb2bd5;
+    bytes32 provenanceGatewaySalt = 0x0000000000000000000000000000000000000000904247ca019d0000ccd75c91;
 
     // Sanity check the calculated addresses.
     address idGateway = 0x000000005F8bda585d7D2b1A0b7e29e12a94910a;
     address usernameGateway = 0x00000000A3B81eB162644186b972C0b6a6f5b8E0;
-    address provenanceGateway = 0x00000000D224D4E84852C3EBE334aE0E914620d3;
+    address provenanceGateway = 0x00000000C8eBA014D75c66d09218728CBa7fB209;
 
     // Calculate initCode hashes.
     function run() external view {
